7691e9fa41e04ed03a958d3fa6c84c69d49a599a
bige3032 <bige3032@163.com>
2019-01-04 10:56:43 +0800

    第一次提交boot项目

---
+++ src/main/java/com/accp/ht/action/big/Sjf.java
@@ -0,0 +1,10 @@
+package com.accp.ht.action.big;
+
+import org.springframework.boot.SpringApplication;
+
+public class Sjf {
+	
+	public static void main(String[] args) {
+		System.out.println("草拟吗");
+	}
+}
