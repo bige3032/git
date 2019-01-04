08f9405883e87d353c98a8603e3789ff3be85365
bige3032 <bige3032@163.com>
2019-01-04 11:15:52 +0800

    git提交1

--- src/main/java/com/accp/ht/action/big/Sjf.java
+++ src/main/java/com/accp/ht/action/big/Sjf.java
@@ -1,10 +1,10 @@
 package com.accp.ht.action.big;
 
-import org.springframework.boot.SpringApplication;
 
 public class Sjf {
 	
 	public static void main(String[] args) {
 		System.out.println("草拟吗");
+		System.out.println("rinima");
 	}
 }
